76be4aa668ccb24e2e236b2428da186ba469ca33
adamklekowski <klekowskiadam@gmail.com>
2020-04-05 17:25:15 +0200

    add comment

--- lab02/usunduplikaty.hs
+++ lab02/usunduplikaty.hs
@@ -1,3 +1,15 @@
+{-
+Napisz funkcję usunduplikaty, która usunie ze stringa powtarzające się znaki.
+Podpowiedzi:
+* string to lista
+* przydatna może być funkcja usuwająca wszystkiego wystąpienia danego elementu z listy
+
+PS zamiast 
+    "usunduplikaty :: [Char] -> [Char]" 
+mogłoby być 
+    "usunduplikaty :: (Eq a) => [a] -> [a]"
+Funkcja byłaby wtedy bardziej uniwersalna
+-}
 usunDuplikaty :: Eq a => [a] -> [a]
 usunDuplikaty [] = []
 usunDuplikaty (x:xs) = x : (usunDuplikaty (usun x xs))
