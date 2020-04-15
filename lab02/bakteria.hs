137479db3d6e37af4947009a6e1923498d727462
adamklekowski <klekowskiadam@gmail.com>
2020-04-15 23:10:47 +0200

    fix bakteria.hs

---
+++ lab02/bakteria.hs
@@ -0,0 +1,13 @@
+{-
+Napisz dwie wersje funkcji bakteria (Druga może nazywać się bakteria') o poniższym typie. 
+Szczegółowe instrukcje na wiki.
+-}
+bakteria :: Int -> (Int, Int)
+bakteria 0 = (1,1)
+bakteria n = (b, 2*a+b) where (a,b) = bakteria(n-1)
+
+bakteria' :: Int -> (Int, Int)
+bakteria' 0 = (2,0)
+bakteria' n = (b, 2*a+b) where (a,b) = bakteria'(n-1)
+
+--map bakteria [0..7]
